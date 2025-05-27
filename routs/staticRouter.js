26210900ce98c02298c4b3b9fed756fc0a3824ba
operation-tss <tssoperation@gmail.com>
2025-05-27 17:51:22 +0530

    add

--- routs/staticRouter.js
+++ routs/staticRouter.js
@@ -3,12 +3,14 @@ const URL = require("../models/url");
 const { restrictTo } = require("../middlewares/auth");
 const router = express.Router();
 
+// 
 router.get("/admin/urls", restrictTo(["NORMAL"]), async (req, res) => {
   const allurls = await URL.find();
   return res.render("home", {
     urls: allurls,
   });
 });
+
 
 router.get("/", restrictTo(["NORMAL", "ADMIN"]), async (req, res) => {
   const allurls = await URL.find({ createdBy: req.user._id });
